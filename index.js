b3a09bb5ffd3d75266b7101c2e0956980b801000
John Doe <johndoe@example.com>
2015-08-20 11:30:36 +0300

    Change script to work with promises

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
 
-BowerRegistry = require('bower-registry-client');
-fs = require('fs');
+var Promise = require("bluebird");
+var execAsync = Promise.promisifyAll(require('child_process')).execAsync;
+var bowerJson = require('bower-json');
+var BowerRegistry = require('bower-registry-client');
+Promise.promisifyAll(BowerRegistry.prototype);
+var bower = new BowerRegistry({ cache: false });
 
-//registry = BowerRegistry({ cache: false });
 
-packageName = process.argv[2];
+packageNames = process.argv.slice(2);
 
-downloadPackage(packageName);
+Promise.all(packageNames.map(downloadPackage)).finally(function () {
+    process.exit();
+});
 
 function downloadPackage(packageName) {
-    fs.exists(packageName, function (exists) {
-        if (!exists) {
-        }
-    })
+    if (downloadPackage.downloaded === undefined) {
+        downloadPackage.downloaded = {};
+    }
+
+    if (downloadPackage.downloaded[packageName] === undefined) {
+        downloadPackage.downloaded[packageName] = 'STARTED';
+        return bower.lookupAsync(packageName).then(function (packageInfo) {
+            return execAsync('git clone ' + packageInfo.url + ' ' + packageName + ' --bare');
+        }).then (function () {
+            return execAsync('git show HEAD:bower.json', { cwd: packageName });
+        }).then(function (stds) {
+            var stdout = stds[0];
+            return JSON.parse(stdout);
+        }).then(function (json) {
+            return bowerJson.parse(json)
+        }).then(function (spec) {
+            var downloads = Object.keys(spec.dependencies).map(function (dependecy) {
+                return downloadPackage(dependecy);
+            });
+            downloadPackage.downloaded[packageName] = 'FINISHED';
+            console.log(packageName, ': Download Finished');
+            return downloads
+        }).spread(function () {
+            return true;
+        }, function (error) {
+            downloadPackage.downloaded[packageName] = 'ERROR';
+            console.error(packageName, ':', error);
+            return false;
+        });
+    }
+    else {
+        return Promise.resolve(true);
+    }
 }
